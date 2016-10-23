77d7dd6c8fb81b47f4e661700c5b0bb3423ab364
Vincent Wu <wuv21@uw.edu>
2016-10-22 21:50:03 -0700

    fixed recording for mobile devices

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	var keysTime = [];
 	var testString = "The quick brown fox jumps over the lazy dog";
+	var testLastChar = testString.charAt(testString.length - 1).toUpperCase();
 
 	function reset() {
 		$('#keyboardInp').val('');
@@ -21,16 +22,16 @@ $(document).ready(function() {
 		keysTime.push(data);
 
 		// test completed
-		if ($(this).val() == testString && keysTime[keysTime.length - 1].keyString == testString.charAt(testString.length - 1)) {
+		if ($(this).val() == testString && keysTime[keysTime.length - 1].keyCode == testLastChar.charCodeAt(0)) {
 			$(this).prop('disabled', true);
 			
 			var parsedData = {
 				duration: keysTime[keysTime.length - 1].time - keysTime[0].time,
 				numKeys: keysTime.length,
-				keysPressed: keysTime.map(function(x) {return x.keyString + ' ' + String(x.time - keysTime[0].time)}),
+				keysPressed: keysTime.map(function(x) {return x.keyString + ' (' + String(x.time - keysTime[0].time) + ')'}),
 			};
 
-			$('#results').val(JSON.stringify(parsedData, null, '\t'));
+			$('#results').text(JSON.stringify(parsedData, null, '\t'));
 		}
 	});
 
