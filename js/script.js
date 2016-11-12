10204a42e5ab6a6b7e10054e5cb8cad6acbfbeb5
studentuser <studentuser@studentusers-MacBook-Pro.local>
2016-11-11 19:16:10 -0800

    begun work on tool for data collection

--- js/script.js
+++ js/script.js
@@ -1,37 +1,58 @@
 $(document).ready(function() {
 	var keysTime = [];
-	var testString = "The quick brown fox jumps over the lazy dog";
+	var testString = "the quick brown fox jumps over the lazy dog";
 	var testLastChar = testString.charAt(testString.length - 1).toUpperCase();
 
 	function reset() {
 		$('#keyboardInp').val('');
 		$('#keyboardInp').prop('disabled', false);
 		$('#results').val('');
+		var container = document.getElementById('fileDownload')
+		container.removeChild(container.childNodes[0]);
 
 		keysTime = [];
 	}
 
+	// todo download static versions of jquery and bootstrap
+
 	// record keystroke data when keyup happens inside textarea
-	$('#keyboardInp').keyup(function(e) {
+	$('#keyboardInp').keypress(function(e) {
+		var timeVanilla = Date.now();
+		var timeQuery = parseInt(e.timeStamp.toFixed(6));
+
 		var data = {
-			time: e.timeStamp,
+			ticks: timeQuery,
+			seconds: timeQuery / 1000,
 			keyCode: e.keyCode,
-			keyString: e.key ? e.key : e.keyCode
+			keyString: e.key ? e.key : e.keyCode,
+			ticksVanilla: timeVanilla,
+			secondsVanilla: timeVanilla / 1000
 		}
 
 		keysTime.push(data);
+		console.log(data);
 
-		// test completed
-		if ($(this).val() == testString && keysTime[keysTime.length - 1].keyCode == testLastChar.charCodeAt(0)) {
+		// test if enter is pressed
+		if (e.keyCode == "13") {
 			$(this).prop('disabled', true);
 			
-			var parsedData = {
-				duration: keysTime[keysTime.length - 1].time - keysTime[0].time,
-				numKeys: keysTime.length,
-				keysPressed: keysTime.map(function(x) {return x.keyString + ' (' + String(x.time - keysTime[0].time) + ')'}),
-			};
+			// var parsedData = {
+			// 	duration: keysTime[keysTime.length - 1].time - keysTime[0].time,
+			// 	numKeys: keysTime.length,
+			// 	keysPressed: keysTime.map(function(x) {return x.keyString + ' (' + x.ticks + ')'}),
+			// };
 
-			$('#results').text(JSON.stringify(parsedData, null, '\t'));
+			// $('#results').text(JSON.stringify(parsedData, null, '\t'));
+
+			var jsonExport = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(keysTime));
+
+			var a = document.createElement('a');
+			a.href = 'data:' + jsonExport;
+			a.download = 'keysTime.json';
+			a.innerHTML = 'download JSON';
+
+			var container = document.getElementById('fileDownload');
+			container.appendChild(a);
 		}
 	});
 
